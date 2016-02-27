6bf775be8ee5e6b91830a746881ee2b0e62ca2ce
Robin Malhotra <malhotra.robin@icloud.com>
2016-02-27 23:29:17 +0530

    begin refactoring

--- newAss2.py
+++ newAss2.py
@@ -12,46 +12,103 @@ class BTreeNode:
     def insert(self,kvPair):
         key = kvPair[0]
         value = kvPair[1]
-        index = len(self.children)
+        index = len(self.values)
         for i,(k,v) in enumerate(self.values):
             if key < k:
                 index = i
                 break
-        if len(self.children)<maxChildren:
+        if len(self.values)==0:
+            self.values.insert(index,(key,value))
+            return
+        if len(self.children)<maxChildren and len(self.values)<maxChildren - 1:
             if len(self.children) == 0:
                 self.values.insert(index,(key,value))
             else:
                 self.children[index].insert((key,value))
         else:
-            newParent = BTreeNode()
-            self.values.insert(index,(key,value))
-            median = self.values[len(self.values)/2]
-            newParent.values = [median]
-            del self.values[index]
+            self.splitIfNecessary((key,value))
+            self.parentNode.getMedianFromChild(median)
 
-            leftNode = BTreeNode()
-            leftNode.values = self.values[:len(self.values)/2]
-            leftNode.children = self.children[:len(self.values)/2]
-            rightNode = BTreeNode()
-            rightNode.values = self.values[len(self.values)/2:]
-            rightNode.children = self.children[len(self.values)/2:]
-            leftNode.parentNode = newParent
-            rightNode.parentNode = newParent
-            newParent.children = [leftNode,rightNode]
-            if self.isRoot == True:
-                self.isRoot = False
-                newParent.isRoot = True
+            # print("<intermediates>")
+            # print(newParent.values)
+            # for child in newParent.children:
+            #     print(child.values)
+            # print("<\intermediates>")
+            #
+    def splitIfNecessary(self,kvPair):
+        key = kvPair[0]
+        value = kvPair[1]
+        if self.isRoot == True:
+            newParent = BTreeNode()
+            self.isRoot = False
+            self.parentNode = newParent
+            newParent.isRoot = True
+
+        self.values.insert(index,(key,value))
+        mid = int(len(self.values)/2) if len(self.values)%2==0 else int(len(self.values)/2) + 1
+        median = self.values[mid]
+        print("median"+ str(median))
+        del self.values[mid]
+        leftNode = BTreeNode()
+        leftNode.values = self.values[:mid]
+        leftNode.children = self.children[:mid]
+        rightNode = BTreeNode()
+        rightNode.values = self.values[mid:]
+        rightNode.children = self.children[mid:]
+        leftNode.parentNode = self.parentNode
+        rightNode.parentNode = self.parentNode
+        # self.parentNode.children = [leftNode,rightNode]
+        index = len(self.parentNode.values)
+        for i,(k,v) in enumerate(self.parentNode.values):
+            if self.values[0][0]<k:
+                index = i
+                break
+        if len(self.parentNode.children)!=0:
+            del self.parentNode.children[index]
+        self.parentNode.children.insert(index,rightNode)
+        self.parentNode.children.insert(index,leftNode)
+        for child in leftNode.children:
+            child.parentNode = leftNode
+        for child in rightNode.children:
+            child.parentNode = rightNode
+
+    def getMedianFromChild(self,median):
+        index = len(self.values)
+        for i,(k,v) in enumerate(self.values):
+            if key < k:
+                index = i
+                break
+        if len(self.values) < maxChildren -1:
+            self.values.insert(index,median)
+        else:
+            splitIfNecessary(median)
+
+    def insertIntoArbit(self,kvPair):
+        key = kvPair[0]
+        value = kvPair[1]
+        index = len(self.values)
+        for i,(k,v) in enumerate(self.values):
+            if key < k:
+                index = i
+                break
+        self.values.insert(index,(key,value))
+
     def search(self,key):
+        index = len(self.values)
         for i,(k,v) in enumerate(self.values):
             if k==key:
                 return self.values
             elif k>key:
-                return self.children[i].search(key)
-        return self.children[-1].search(key)
-    def inOrderTraverse(self):
-        for i,(k,v) in self.values:
-            print(self.children[i].inOrderTraverse())
-            print(self.values[i])
+                index = i
+                break
+        if len(self.children) == 0:
+            return None
+        else:
+            return self.children[index].search(key)
+    # def inOrderTraverse(self):
+    #     for i,(k,v) in enumerate(self.values):
+    #         print(self.children[i].inOrderTraverse())
+    #         print(self.values[i])
 
 
 class BTree:
@@ -65,12 +122,18 @@ class BTree:
             node = BTreeNode()
             node.isRoot = True
             self.root = node
-            node.values = kvPair
+            node.values = [kvPair]
             return
         if preSearchNode != None:
             preSearchNode[1].append(value)
         else:
             self.root.insert((key, value))
+            if not self.root.isRoot:
+                print("resetting root from "+ str(self.root.values) + "to " + str(self.root.parentNode.values))
+                x = self.root
+                self.root = self.root.parentNode
+                del x
+
     def search(self,key):
         if self.root!=None:
             return self.root.search(key)
@@ -81,10 +144,13 @@ class BTree:
 
 def main():
     tree = BTree()
-    tree.insert((10,[10]))
-    tree.insert((3,[3]))
-    tree.insert((4,[4]))
-    tree.insert((5,[5]))
+    for i,(k,v) in enumerate([(x,[x]) for x in range(1,8)]):
+        print("inserting "+ str((k,v)))
+        tree.insert((k,v))
+    print("results:")
+    print(tree.root.values)
+    for child in tree.root.children:
+        print(child.values)
     # root = BTreeNode()
     # tree.root = root
     # root.values = [(10,[10])]
@@ -113,6 +179,6 @@ def main():
     # child1.children = [grand1,grand2,grand3,grand4]
     #
     # x = tree.search(7)
-    print(tree.root.isRoot)
+    # print(tree.root.isRoot)
 
 main()
