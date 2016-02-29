0328c41c2e9959e546e071cd750f39f7a3adbf0d
Robin Malhotra <malhotra.robin@icloud.com>
2016-02-29 14:37:39 +0530

    done

--- newAss2.py
+++ newAss2.py
@@ -1,3 +1,11 @@
+import sys
+import json
+import re
+from prettytable import PrettyTable
+schemas = {}
+records = {}
+relations = []
+BTrees = {}
 minChildren = 2
 maxChildren = 4
 class BTreeNode:
@@ -47,7 +55,6 @@ class BTreeNode:
         mid = int(len(self.values)/2) if len(self.values)%2==0 else int((len(self.values)+1)/2)
         midChildren = int(len(self.children)/2) if len(self.children)%2==0 else int((len(self.children)+1)/2)
         median = self.values[mid]
-        print("median"+ str(median))
         del self.values[mid]
         leftNode = BTreeNode()
         leftNode.values = self.values[:mid]
@@ -113,24 +120,29 @@ class BTreeNode:
             return self.children[index].search(key)
 
     def remove(self,key):
+        print(key)
         for i,(k,v) in enumerate(self.values):
             if k == key:
-                if len(children) == 0:
+                if len(self.children) == 0 and len(self.values)>1:
                     self.values.pop(i)
                     return
                 else:
-                    (successorNode,successorIndex) = self.getSuccessor(key)
-                    self.values[i] = self.successorNode.values[successorIndex]
+                    if len(self.values) == 1: ##underflowing leaf
+                        successorNode = self
+                        successorIndex = 0
+                    else:
+                        (successorNode,successorIndex) = self.getSuccessor(key)
+                    self.values[i] = successorNode.values[successorIndex]
                     if len(successorNode.values)>1:
                         successorNode.values.pop(successorIndex)
                     else:
                         leftSibling, rightSibling = self.getSibilings()
-                        if leftSibling and len(self.leftSibling.values)>1:
+                        if leftSibling and len(leftSibling.values)>1:
                             successorNode.performLeftRotation(leftSibling,successorIndex)
-                        elif rightSibling and len(self.rightSibling.values)>1:
+                        elif rightSibling and len(rightSibling.values)>1:
                             successorNode.performRightRotation(rightSibling,successorIndex)
                         else:
-                            successorNode.performFuses(leftSibling,rightSibling,successorIndex)
+                            successorNode.performFuses()
                     return
             elif k>key:
                 return self.children[i].remove(key)
@@ -138,46 +150,70 @@ class BTreeNode:
 
     def performLeftRotation(self,leftSibling,successorIndex):
         index = 0
-        for i,c in self.parentNode.children:
+        for i,c in enumerate(self.parentNode.children):
             if c == self:
                 index = i
                 break
         parentPair = self.parentNode.values[index - 1]
-        self.parentNode.values[i] = leftSibling.values.pop()
-        self.values[successorIndex] = parentPair
+        self.parentNode.values[i - 1] = leftSibling.values.pop()
+        if self.children != []:
+            self.children.insert(0,leftSibling.children.pop())
+        if len(self.values)!=0:
+            self.values[successorIndex] = parentPair
+        else:
+            self.values.insert(0,parentPair)
 
     def performRightRotation(self,rightSibling,successorIndex):
         index = 0
-        for i,c in self.parentNode.children:
+        for i,c in enumerate(self.parentNode.children):
             if c == self:
                 index = i
                 break
         parentPair = self.parentNode.values[index - 1]
         self.parentNode.values[i] = rightSibling.values.pop(0)
-        self.values[successorIndex] = parentPair
+        if self.children != []:
+            self.children.push(leftSibling.children.pop(0))
+        if len(self.values)!=0:
+            self.values[successorIndex] = parentPair
+        else:
+            self.values.insert(0,parentPair)
 
-    def performFuses(self,successorIndex):
+    def performFuses(self):
+
+        print(self.parentNode.values)
         leftSibling, rightSibling = self.getSibilings()
         index = 0
-        for i,c in self.parentNode.children:
+        for i,c in enumerate(self.parentNode.children):
             if c == self:
                 index = i
                 break
         parentPair = self.parentNode.values[index - 1]
-        self.values.pop(successorIndex)
         if leftSibling:
             self.parentNode.children.pop(index)
             leftSibling.values.append(parentPair)
-            self.parentNode.remove(parentPair[0])
+
         elif rightSibling:
             self.parentNode.children.pop(index)
-            leftSibling.values.insert(0,parentPair)
-            self.parentNode.remove(parentPair[0])
+            rightSibling.values.insert(0,parentPair)
 
-        if self.parent.isRoot and len(self.parent.values) == 0:
-            self.isRoot = True
-            self.parent.isRoot = False
-            self.parent = None
+        self.parentNode.values.pop(index - 1)
+        if len(self.parentNode.values) == 0:
+            parent = self.parentNode
+            if not self.parentNode.isRoot:
+                leftSibling, rightSibling = self.parentNode.getSibilings()
+                print(leftSibling.values)
+                if leftSibling and len(leftSibling.values)>1:
+                    self.parentNode.performLeftRotation(leftSibling,0)
+                elif rightSibling and len(rightSibling.values)>1:
+                    self.parentNode.performRightRotation(rightSibling,0)
+                else:
+                    self.parentNode.performFuses()
+
+            else:
+                self.isRoot = True
+                self.parentNode.isRoot = False
+                self.parent = None
+
 
     def getSibilings(self):
         for i,c in enumerate(self.parentNode.children):
@@ -241,13 +277,106 @@ class BTree:
     def inOrderTraverse(self):
         self.root.inOrderTraverse()
 
+
+def removeFirst(arr):
+	return arr[1:]
+
+
 def main():
+    if len(sys.argv) != 3:
+        raise ValueError('You done screwed up')
+
+    # with open(sys.argv[1]) as f:
+    #     content = [x[:-1] for x in f.readlines()]
+    #     numberOfTables = int(content[0][1:])
+    #     content = removeFirst(content)
+    #     for i in range(0,numberOfTables):
+    #         nameOfTable = content[0]
+    #         schemas[nameOfTable] = {}
+    #         records[nameOfTable] = []
+    #         content = removeFirst(content)
+    #         numberOfAttrs = int(content[0][1:])
+    #         content = removeFirst(content)
+    #         attrNames = content[0].split(',')
+    #         content = removeFirst(content)
+    #         schemas[nameOfTable] = attrNames
+    #         BTrees[nameOfTable] = {}
+    #         for attr in attrNames:
+    #             BTrees[nameOfTable][attr] = BTree()
+    #         numberOfRecords = int(content[0][1:])
+    #         content = removeFirst(content)
+    #         for j in range(0, numberOfRecords):
+    #         	dic = {}
+    #         	tup = content[j].split(',')
+    #         	for k,attr in enumerate(attrNames):
+    #                 if k==0:
+    #                     BTrees[nameOfTable][attr].insert((int(tup[k]),int(tup[k])))
+    #                 dic[attr] = tup[k]
+    #         	records[nameOfTable].append(dic)
+    #         content = content[numberOfRecords:]
+    #         content = removeFirst(content)
+    #     content = removeFirst(content)
+    #     # print(json.dumps(records,sort_keys=True, indent=4))
+    #     # print(json.dumps(schemas,sort_keys=True, indent=4))
+    #     print(content==[])
+    #     root = BTrees["S"]["sid"].root
+    #     i=0
+    #     while(len(root.children)!=0):
+    #         print(i)
+    #         i+=1
+    #         root = root.children[0]
+    #     print(i)
+    #
+    # with open(sys.argv[2]) as f:
+    #     content = [x[:-1] for x in f.readlines()]
+    #     for query in content:
+    #         tup = query.split(" ")
+    #         if tup[0] == "insert":
+    #             nameOfTable = tup[1]
+    #             values = tup[2].split(",")
+    #             dic = {}
+    #             attrNames = schemas[nameOfTable].keys()
+    #             for i,value in enumerate(values):
+    #                 dic[attrNames[i]] = value
+    #
+    #             records[nameOfTable].append(dic)
+    #         elif tup[0] == "update":
+    #             pass
+    #         elif tup[0] == "delete":
+    #             pass
+    #         else:
+    #             query = query[8:-1]
+    #             regexPattern = '|'.join(map(re.escape, ['(',')']))
+    #             subqueries = [x for x in re.split(regexPattern,query) if x!='']
+    #             print(subqueries)
+    #             for subquery in subqueries:
+    #                 pass
+    #
+    #
+    #
+
     tree = BTree()
     for i,(k,v) in enumerate([(x,[x]) for x in range(1,17)]):
         print("inserting "+ str((k,v)))
         tree.insert((k,v))
     print(tree.root.values)
     tree.insert((16,1024))
+    for child in tree.root.children:
+        print("child")
+        print(child.values)
+        for grandChild in child.children:
+            print("grandChildchild")
+            print(grandChild.values)
+    tree.remove(16)
+    tree.remove(15)
+    tree.remove(14)
+    tree.remove(13)
+    tree.remove(12)
+    tree.remove(11)
+    tree.remove(10)
+    tree.remove(9)
+    print("AFTER REMOVAL\n\n\n")
+    print(tree.root.values)
     for child in tree.root.children:
         print("child")
         print(child.values)
